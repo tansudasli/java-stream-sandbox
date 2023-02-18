ce5f15cbbf737ca528998e304f59da8c02fd54a7
tansudasli <tansudasli@gmail.com>
2023-02-18 13:06:17 +0300

    Multithreading injections : restructuring

--- core/src/jmh/java/org/core/TasksShoutBenchmark.java
+++ core/src/jmh/java/org/core/TasksShoutBenchmark.java
@@ -8,7 +8,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
-public class TasksBenchmark {
+public class TasksShoutBenchmark {
 
     private final static int LAST = 1000000;
     private final static int FIRST = 0;
@@ -43,14 +43,14 @@ public class TasksBenchmark {
         System.out.println("..." + Thread.currentThread());
 
         var opt = new OptionsBuilder()
-                .include(TasksBenchmark.class.getName())
+                .include(TasksShoutBenchmark.class.getName())
                 .jvmArgs("-Xms1g", "-Xmx1g", "-XX:+UseG1GC")
 //                .threads(2)   //increases jmh-threads, if your test is already multithreaded, it may badly affect your perf.
                 .warmupIterations(1)
                 .measurementIterations(1)
                 .forks(1)
 //                .resultFormat(ResultFormatType.JSON)
-//                .result("build/".concat(org.core.TasksBenchmark.class.getName()).concat(".json"))
+//                .result("build/".concat(org.core.TasksShoutBenchmark.class.getName()).concat(".json"))
                 .build() ;
 
         new Runner(opt).run() ;
@@ -61,8 +61,8 @@ public class TasksBenchmark {
 
     //last = 1m
     //Benchmark                                           Mode  Cnt      Score   Error   Units
-    //TasksBenchmark.testTaskTraditionalForMultiThread   thrpt          ≈ 10⁻⁴          ops/ms
-    //TasksBenchmark.testTaskTraditionalForSingleThread  thrpt          ≈ 10⁻⁴          ops/ms
-    //TasksBenchmark.testTaskTraditionalForMultiThread    avgt       12491.590           ms/op
-    //TasksBenchmark.testTaskTraditionalForSingleThread   avgt       11827.027           ms/op
+    //TasksShoutBenchmark.testTaskTraditionalForMultiThread   thrpt          ≈ 10⁻⁴          ops/ms
+    //TasksShoutBenchmark.testTaskTraditionalForSingleThread  thrpt          ≈ 10⁻⁴          ops/ms
+    //TasksShoutBenchmark.testTaskTraditionalForMultiThread    avgt       12491.590           ms/op
+    //TasksShoutBenchmark.testTaskTraditionalForSingleThread   avgt       11827.027           ms/op
 }
