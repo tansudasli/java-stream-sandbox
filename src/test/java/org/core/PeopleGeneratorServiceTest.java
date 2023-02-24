6bea4c2de10126671c1daf8aa52f6dc4301d0973
tansudasli <tansudasli@gmail.com>
2023-02-24 23:55:39 +0300

    average w/ reduce

--- src/test/java/org/core/PeopleGeneratorServiceTest.java
+++ src/test/java/org/core/PeopleGeneratorServiceTest.java
@@ -3,6 +3,8 @@ package org.core;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PeopleGeneratorServiceTest {
@@ -11,7 +13,7 @@ class PeopleGeneratorServiceTest {
     static String PEOPLE_FIRST = "James";
 
     @Test
-    @DisplayName("Test employee count == 122")
+    @DisplayName("Test employee count == 4")
     void checkPeopleCount() {
         assertEquals(PEOPLE_COUNT, IPeopleGeneratorService.of.get().size());
     }
@@ -19,9 +21,27 @@ class PeopleGeneratorServiceTest {
     @Test
     @DisplayName("Test 1st employee is Ali")
     void checkFirstPeople() {
-        assertEquals(PEOPLE_FIRST, IPeopleGeneratorService.of.get().stream()
-                .findFirst()
-                .get()
-                .firstName());
+        assertEquals(PEOPLE_FIRST,
+                IPeopleGeneratorService.of.get().stream()
+                        .findFirst()
+                        .get()
+                        .firstName());
+    }
+
+    @Test
+    void averageAge(){
+        Supplier<IllegalArgumentException> missingAgeException = () -> new IllegalArgumentException("age missing!");
+
+        var total = IPeopleGeneratorService.of.get()
+                .stream()
+                .map(Person::age)
+                .reduce(Integer::sum)
+                .orElseThrow(missingAgeException);
+
+
+        var count = (long) IPeopleGeneratorService.of.get().size();
+
+        System.out.printf("Age of Σ=%d | size=%d | average=%d\n", total, count, total/count);
+
     }
 }
