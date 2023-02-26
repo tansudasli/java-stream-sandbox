1f0c685e20a4af3084eefc001c5623fbd26ac740
tansudasli <tansudasli@gmail.com>
2023-02-26 23:43:02 +0300

    added flatMap

--- src/test/java/org/core/PeopleGeneratorServiceTest.java
+++ src/test/java/org/core/PeopleGeneratorServiceTest.java
@@ -3,6 +3,7 @@ package org.core;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,35 +12,46 @@ class PeopleGeneratorServiceTest {
 
     static int PEOPLE_COUNT = 4;
     static String PEOPLE_FIRST = "James";
+    List<Person> people = IPeopleGeneratorService.of.get();
 
     @Test
     @DisplayName("Test employee count == 4")
     void checkPeopleCount() {
-        assertEquals(PEOPLE_COUNT, IPeopleGeneratorService.of.get().size());
+        assertEquals(PEOPLE_COUNT, people.size());
     }
 
     @Test
     @DisplayName("Test 1st employee is Ali")
     void checkFirstPeople() {
-        assertEquals(PEOPLE_FIRST,
-                IPeopleGeneratorService.of.get().stream()
-                        .findFirst()
-                        .get()
-                        .firstName());
+        assertEquals(PEOPLE_FIRST, people.stream()
+                                            .findFirst()
+                                            .get()
+                                            .firstName());
+    }
+
+    @Test
+    void printPhones() {
+//flatmap Stream<Stream<T>>
+
+        people.stream()
+                .flatMap((person -> person.phones().stream()))
+                .forEach(System.out::println);
+
+        people.stream()
+                .forEach(System.out::println);
     }
 
     @Test
     void averageAge(){
         Supplier<IllegalArgumentException> missingAgeException = () -> new IllegalArgumentException("age missing!");
 
-        var total = IPeopleGeneratorService.of.get()
-                .stream()
-                .map(Person::age)
-                .reduce(Integer::sum)
-                .orElseThrow(missingAgeException);
+        var total = people.stream()
+                        .map(Person::age)
+                        .reduce(Integer::sum)
+                        .orElseThrow(missingAgeException);
 
-
-        var count = (long) IPeopleGeneratorService.of.get().size();
+//Todo: make this reduce w/ combiner !
+        var count = (long) people.size();
 
         System.out.printf("Age of Σ=%d | size=%d | average=%f\n", total, count, (double)total/count);
 
