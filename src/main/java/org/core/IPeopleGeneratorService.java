2b084f71be46f0befb92f088b1269b747168aa64
tansudasli <tansudasli@gmail.com>
2023-03-01 15:22:18 +0300

    Immutable data creation

--- src/main/java/org/core/IPeopleGeneratorService.java
+++ src/main/java/org/core/IPeopleGeneratorService.java
@@ -1,21 +1,25 @@
 package org.core;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Supplier;
 
 public interface IPeopleGeneratorService {
 
-    List<Person> data = Arrays.asList(
+//    List<Person> data = Arrays.asList(
+//            new Person(1, "James", "Williams", "James@hotmail.com", "Male",44, Arrays.asList("1-857-545-5925","1-894-887-1309")),
+//            new Person(2,"Ezekiel","Haynes","ezekiel8216@icloud.net","Male", 32, Arrays.asList("1-914-177-5177","1-286-471-5016")),
+//            new Person(453,"Kirestin","Stevenson","kirestin@outlook.couk","Female",41,Arrays.asList("1-260-278-4847","1-368-942-5288")),
+//            new Person(454,"Camilla","Contreras","camilla@aol.net","Female",19,Arrays.asList("1-636-524-5164","1-662-329-2646"))
+//    );
+
+    //new ArrayList<>() --- mutable version
+    //immutable creation
+    List<Person> data = List.of(
             new Person(1, "James", "Williams", "James@hotmail.com", "Male",44, Arrays.asList("1-857-545-5925","1-894-887-1309")),
             new Person(2,"Ezekiel","Haynes","ezekiel8216@icloud.net","Male", 32, Arrays.asList("1-914-177-5177","1-286-471-5016")),
             new Person(453,"Kirestin","Stevenson","kirestin@outlook.couk","Female",41,Arrays.asList("1-260-278-4847","1-368-942-5288")),
             new Person(454,"Camilla","Contreras","camilla@aol.net","Female",19,Arrays.asList("1-636-524-5164","1-662-329-2646"))
     );
-
-
 
     Supplier<List<Person>> of = () -> data;
 
