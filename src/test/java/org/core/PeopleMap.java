58bc1d93f8aa4bd8812bad67740c2e8895a34229
tansudasli <tansudasli@gmail.com>
2023-03-01 15:14:34 +0300

    Map<,> from stream

---
+++ src/test/java/org/core/PeopleMap.java
@@ -0,0 +1,24 @@
+package org.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PeopleMap {
+
+    Map<Integer, Person> dataMap = IPeopleGeneratorService.of.get()
+                                           .stream()
+                                           .collect(Collectors.toMap(Person::id, person -> person));
+
+    @Test
+    void printDataMap() {
+
+//        System.out.println(dataMap);
+
+        dataMap.forEach((key, person) -> System.out.println(String.join("=",
+                                                                          key.toString(),
+                                                                          person.firstName())));
+    }
+
+}
