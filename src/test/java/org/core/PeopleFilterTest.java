2a7c6b2c7617c778dbd7ecfba25288e7dd4bbbf0
tansudasli <tansudasli@gmail.com>
2023-03-01 16:19:13 +0300

    added query by age & predicate concatenation

---
+++ src/test/java/org/core/PeopleFilterTest.java
@@ -0,0 +1,20 @@
+package org.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+public class PeopleFilterTest {
+
+    @Test
+    void queryByAgeGreaterThan20() {
+        Predicate<Person> isAgeGreaterThan20 = person -> person.age() > 20;
+        Predicate<Person> isFemale = person -> person.gender().equals("Female");
+
+        IPeopleGeneratorService.of.get()
+                .stream()
+                .filter(isAgeGreaterThan20
+                        .and(isFemale))         //concatenation
+                .forEach(System.out::println);
+    }
+}
