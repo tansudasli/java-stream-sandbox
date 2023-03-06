a170e5dedb480c336ba1c49e51d9a7fb80cd2c22
tansudasli <tansudasli@gmail.com>
2023-03-06 16:52:56 +0300

    Collectors vs other way...

---
+++ src/test/java/org/core/StreamsInDeepTest.java
@@ -0,0 +1,43 @@
+package org.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class StreamsInDeepTest {
+    //understanding streams
+
+    @BeforeEach
+    void before() {
+        System.out.println("start:::".concat(Thread.currentThread().getName()));
+    }
+
+    @Test
+    void test() {
+        emailsZ.get()
+                .forEach(s -> {
+                    System.out.println("forEach:::"
+                                       .concat(Thread.currentThread().getName())
+                                       .concat("::: ")
+                                       .concat(s));
+
+                });
+    }
+
+    Supplier<List<String>> emailsZ =
+            () -> { return
+                IPeople.people
+                        .parallelStream()
+                        .map(person -> {
+                            System.out.println("map:::".concat(Thread.currentThread().getName()));
+
+                            return person.email();
+                        })
+                        .collect(ArrayList::new,
+                                 ArrayList::add,
+                                 ArrayList::addAll);
+            };
+}
