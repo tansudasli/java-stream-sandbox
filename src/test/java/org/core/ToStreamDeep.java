5852443dec81e1f91234dfdab6ae33c326152b76
tansudasli <tansudasli@gmail.com>
2023-03-06 23:24:44 +0300

    restructuring

--- src/test/java/org/core/ToStreamDeep.java
+++ src/test/java/org/core/ToStreamDeep.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class StreamsInDeepTest {
+public class ToStreamDeep {
     //understanding streams
 
     @BeforeEach
